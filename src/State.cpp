7c629ae0958a28c9efb50f38df4251815050f9d1
Migou <migouste@yahoo.fr>
2016-01-24 21:20:30 +0100

    les fichiers manquants

--- src/State.cpp
+++ src/State.cpp
@@ -1,11 +1,26 @@
+using namespace std;
 à mettre dans State.h
+
 class State
 {
 private :
-  vector<LogicalBoard> logicalBoardList;
-  vector<Piece> piecesList;
+  //vector<LogicalBoard> logicalBoardList;
+  //vector<Piece> piecesList;
+  int nbalumettes=10; // pour le test
+  int num_joueur=1;  // pour le test
 Public :
+  State(string luaConfFile);
+  State(Coup coupJoue)
 
+  // la meme mais on n'instancie pas de nouvel état (il faudra stocker les infos dans une pile de facon à pouvoir revenir à l'état précédent.
+  void State::update(Coup coupJoue);
+  void State::rollBackCoup();
+  //  vector<LogicalBoard> State::getBoardList();
+  // vector<Piece> State::getPieceList();
+  vector<Coup> State::getCoupsPossibles();
+  bool State::partieNulle();
+	// si un des joueurs est gagnant dans la position actuelle, renvoie un pointeur vers l'heure gagnant. Sinon renvoie null (ex : aux echecs, il a bloqué l'adversaire et il met son roi en echec)
+  Player* State::getGagnant();
 }
 
 ---8<---
@@ -41,14 +56,23 @@ vector<Piece> State::getPieceList(){
     }
 
 vector<Coup> State::getCoupsPossibles(){
-    !!!
-      }
+  vector<Coup>* res = new vector<Coup>();
+  for(int i = 1; i <= this->nballumettes; i++ ){
+    res.pushback(i);
+  }
+  return res;
+}
 
+
+bool State::partieNulle(){
+  return false;
+}
   // si un des joueurs est gagnant dans la position actuelle, renvoie un pointeur vers l'heure gagnant. Sinon renvoie null (ex : aux echecs, il a bloqué l'adversaire et il met son roi en echec)
 Player* State::getGagnant()
 {
-  return 0;!!!
-	     }
+  int* i = malloc(sizeof(int));
+}
+
 
   
 
