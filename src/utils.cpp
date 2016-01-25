9c7928afd8bd968b37089eb19bda66cfaefe4e1f
Migou <migouste@yahoo.fr>
2016-01-25 22:12:24 +0100

    ajout du fichier utils.cpp pas à jour

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,20 @@
-#include<stdlib.h> // exit
-#include<iostream>
+#include <stdlib.h> // exit
+#include <iostream>
+#include <string>
+#include <sstream> // int 2 string conversion
 using namespace std;
-
 
 void die(string mess){
   cerr << mess << endl;
   exit(1);
+}
+
+string my_int_to_string(int i)
+{
+string res;          // string which will contain the result
+ostringstream convert;   // stream used for the conversion
+convert << i;      // insert the textual representation of 'Number' in the characters in the stream
+
+return convert.str(); // set 'Result' to the contents of the stream
 }
 
